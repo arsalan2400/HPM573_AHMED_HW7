f63b56fadd35a1b8e69d84708011aabb3e0e7dc5
Arsalan Ahmed <35544234+arsalan2400@users.noreply.github.com>
2018-03-30 16:00:35 -0400

    Update ArsalanAhmedWeek7.2 and 7.3.py

--- ArsalanAhmedWeek7.2 and 7.3.py
+++ ArsalanAhmedWeek7.2 and 7.3.py
@@ -1,9 +1,10 @@
 print('Question 7.2')
 print('We know, based on the >5 year survivors, this is Binomial Dist with parameters: N= 573 patients, p= about 0.42, q = about 0.58')
+#one run gave me 0.5863874345549738; we'll go with it. 
 print('\n')
 print('Question 7.3')
 import scipy.stats as stat
-prob=stat.binom._pmf(400, 573, 0.58)
-#I got about 0.58 for the prob from my single trial.
+prob=stat.binom._pmf(400, 573, 0.5863874345549738)
+#I got 0.5863874345549738 in a run for the prob from my single trial.
 print('The chances of 400/573 people surviving @ 5 year end if 50% of the patients in our simulated cohort survived beyond 5 years is...', prob)
 
